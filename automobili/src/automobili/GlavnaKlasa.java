31ca4a8e9f57ce3a2447d30de9c486ca0b0b69ab
U4 OIK PC1 <U4 OIK PC1@DESKTOP-S3Q85RQ>
2019-04-11 20:04:03 +0200

    dodali smo fiat

--- automobili/src/automobili/GlavnaKlasa.java
+++ automobili/src/automobili/GlavnaKlasa.java
@@ -6,8 +6,8 @@ public class GlavnaKlasa {
 
 	public static void main(String[] args) {
 		
-		
-		
+		Metode metode=new Metode();
+		metode.ubaciMarku("fijat", "italija");
 
 	}
 
--- automobili/src/automobili/Metode.java
+++ automobili/src/automobili/Metode.java
@@ -2,6 +2,7 @@ package automobili;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Metode {
@@ -20,7 +21,41 @@ public class Metode {
 		
 	}
 	
-	
+	public void ubaciMarku(String nazivMarke,String zemlja) {
+		
+		Connection konekcija=null;
+		PreparedStatement pst=null;
+		
+		try {
+			konekcija=konektujSe("automobili");
+			String query ="insert into marke values(null,?,?)";
+			pst=konekcija.prepareStatement(query);
+			pst.setString(1, nazivMarke);
+			pst.setString(2, zemlja);
+			pst.execute();
+			System.out.println("Uspesan unos");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Neuspesan unos");
+		}finally {
+			
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				konekcija.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
 	
 
 }
